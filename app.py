776b88ff599209806ba7d37b957eccd676cc4b9f
Bogdan Akhmedov <bogdan.akhmedov.te13@sodertalje.ntig.se>
2016-02-10 13:45:51 +0100

    added first flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+__author__ = 'bogdan'
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def powerswitch():
+    return 'powerswitch!'
+
+if __name__ == '__main__':
+    app.run()
